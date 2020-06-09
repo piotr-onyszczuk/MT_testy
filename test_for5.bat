c5d5e6f64f252ba7c97d0b3e1e689f1fa5c5cac2
Michał Milewski <miles98@interia.pl>
2020-06-10 00:21:13 +0200

    Dodanie podziału na grupy ABC i full5

---
+++ test_for5.bat
@@ -0,0 +1,37 @@
+@Echo Off
+SetLocal EnableDelayedExpansion
+
+for /f "tokens=*" %%a in (tests_for5.txt) do (
+    set /A problem = 0
+	
+    %1 tests\%%a
+	
+    if !errorlevel! == 0 (
+		ilasm tests\%%a.il
+		if !errorlevel! == 0 (
+			peverify tests\%%a.exe 
+			if !%errorlevel! == 0 (
+				tests\%%a.exe > results\%%a
+			) else (
+				set /A problem = 1
+			)
+		) else (
+			set /A problem = 1
+		)
+	) else (
+		set /A problem = 1
+	)
+	
+	if !problem! == 1 (
+		echo error> results\%%a
+	)
+)
+
+echo errors detected > errors_for5.txt
+for /f "tokens=*" %%a in (tests_for5.txt) do (
+
+	fc results\%%a expected_results\%%a
+	if not !errorlevel! == 0 (
+		fc results\%%a expected_results\%%a >> errors.txt
+	)
+)
---
+++ test_groupC.bat
@@ -0,0 +1,37 @@
+@Echo Off
+SetLocal EnableDelayedExpansion
+
+for /f "tokens=*" %%a in (tests_groupC.txt) do (
+    set /A problem = 0
+	
+    %1 tests\%%a
+	
+    if !errorlevel! == 0 (
+		ilasm tests\%%a.il
+		if !errorlevel! == 0 (
+			peverify tests\%%a.exe 
+			if !%errorlevel! == 0 (
+				tests\%%a.exe > results\%%a
+			) else (
+				set /A problem = 1
+			)
+		) else (
+			set /A problem = 1
+		)
+	) else (
+		set /A problem = 1
+	)
+	
+	if !problem! == 1 (
+		echo error> results\%%a
+	)
+)
+
+echo errors detected > errors_groupC.txt
+for /f "tokens=*" %%a in (tests_groupC.txt) do (
+
+	fc results\%%a expected_results\%%a
+	if not !errorlevel! == 0 (
+		fc results\%%a expected_results\%%a >> errors.txt
+	)
+)
