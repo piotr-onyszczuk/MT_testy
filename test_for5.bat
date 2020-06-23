9647f28a374e8cf624d96b19f62b085610b4376a
Bartlomiej Szczyglo <bszczyglo.incoming@fh-salzburg.ac.at>
2020-06-23 23:53:10 +0200

    Liczenie ile testow przeszlo i dokladniejsze info o typie bledu

--- test_for5.bat
+++ test_for5.bat
@@ -19,17 +19,17 @@ for /f "tokens=*" %%a in (tests_for5.txt) do (
 					tests\%%a.exe > results\%%a
 				)
 				if NOT !%errorlevel! == 0 (
-					set /A problem = 2
+					set /A problem = 4
 					)
 			) else (
-				set /A problem = 2
+				set /A problem = 3
 			)
 		) else (
 			set /A problem = 2
 		)
 	) else (
 		if !errorlevel! LSS 0 (
-			set /A problem = 3
+			set /A problem = 5
 		) else (
 		set /A problem = 1
 		)
@@ -39,22 +39,34 @@ for /f "tokens=*" %%a in (tests_for5.txt) do (
 		echo error> results\%%a
 	)
 	if !problem! == 2 (
-		echo critical_error> results\%%a
+		echo ilasm_error> results\%%a
+	)
+	if !problem! == 3 (
+		echo peverify_error> results\%%a
+	)
+	if !problem! == 4 (
+		echo different_error> results\%%a
 	)
 	
-	if !problem! == 3 (
+	if !problem! == 5 (
 		echo compiler_error> results\%%a
 	)
 )
 
 echo errors detected > errors_for5.txt
+set ers=0
+set total=0
 for /f "tokens=*" %%a in (tests_for5.txt) do (
 
 	fc results\%%a expected_results\%%a
 	if not !errorlevel! == 0 (
 		fc results\%%a expected_results\%%a >> errors_for5.txt
+		set /a ers=ers+1
 	)
+	set /a total=total+1
 )
+echo TOTAL ERRORS:  %ers% / %total%
+echo TOTAL ERRORS:  %ers% / %total% >> errors_for5.txt
 
 call test.bat %1
 call test_groupA.bat %1
